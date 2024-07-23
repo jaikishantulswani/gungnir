00943f1b864dcdb9aee29c38e3dceb563d7cccb0
Jaikishan Tulswani <30144126+jaikishantulswani@users.noreply.github.com>
2024-07-23 21:33:36 +0530

    Update options.go
    added argument to control concurrency and fetching log time in seconds

--- pkg/runner/options.go
+++ pkg/runner/options.go
@@ -3,10 +3,12 @@ package runner
 import "flag"
 
 type Options struct {
-	Verbose    bool
-	RootList   string
-	Debug      bool
-	JsonOutput bool
+	Verbose     bool
+	RootList    string
+	Debug       bool
+	JsonOutput  bool
+	Concurrency int
+	RateLimit   int // New field for rate limit in seconds
 }
 
 func ParseOptions() *Options {
@@ -16,6 +18,8 @@ func ParseOptions() *Options {
 	flag.BoolVar(&options.Verbose, "v", false, "Output go logs (500/429 errors) to command line")
 	flag.BoolVar(&options.Debug, "debug", false, "Debug CT logs to see if you are keeping up")
 	flag.BoolVar(&options.JsonOutput, "j", false, "JSONL output cert info")
+	flag.IntVar(&options.Concurrency, "concurrency", 5, "Number of concurrent workers")
+	flag.IntVar(&options.RateLimit, "seconds", 120, "Rate limit in seconds for log fetching")
 	flag.Parse()
 
 	return options
